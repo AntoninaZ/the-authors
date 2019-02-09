da109e712617311aec364d90c0719a2a1fee375c
Antonina <zuch21@gmail.com>
2019-02-09 12:32:04 +0200

    CREATE: create project

---
+++ src/elements/List/List.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import cn from 'classnames';
+import styles from './List.jss';
+
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+
+    return;
+  }
+}
+
+export default List;
---
+++ src/elements/Listitem/ListItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import cn from 'classnames';
+import styles from './ListItem.jss';
+
+
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+
+    return;
+  }
+}
+
+export default ListItem;
