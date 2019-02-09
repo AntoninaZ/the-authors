95c58432e0e09537e4be249362aadac2229b28b3
Antonina <zuch21@gmail.com>
2019-02-09 21:15:39 +0200

    Updates

---
+++ precache-manifest.23838f62a062013bb4fd718b39896ef6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "98642d3cceaaf0e9bbe88916d5c2f82e",
+    "url": "/the-authors/static/media/3rd.98642d3c.svg"
+  },
+  {
+    "revision": "74629788d1ed8d1336b2899fe54d2db0",
+    "url": "/the-authors/static/media/2nd.74629788.svg"
+  },
+  {
+    "revision": "86c17a7097b302da04a1ab845ebec5ee",
+    "url": "/the-authors/static/media/1st.86c17a70.svg"
+  },
+  {
+    "revision": "516d3bb90e33a73ca6ff",
+    "url": "/the-authors/static/js/runtime~main.516d3bb9.js"
+  },
+  {
+    "revision": "e164ef5c1a54e77baf39",
+    "url": "/the-authors/static/js/main.e164ef5c.chunk.js"
+  },
+  {
+    "revision": "ff6be69f746f9ade86ad",
+    "url": "/the-authors/static/js/1.ff6be69f.chunk.js"
+  },
+  {
+    "revision": "e164ef5c1a54e77baf39",
+    "url": "/the-authors/static/css/main.a52ea1ea.chunk.css"
+  },
+  {
+    "revision": "56f07e3f284594a6da6a3ba8e02010c4",
+    "url": "/the-authors/index.html"
+  }
+];
